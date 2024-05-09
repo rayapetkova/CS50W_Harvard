2c5cd90421cc12efafab1c58f8779d92677106c6
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-05-09 22:54:27 +0300

    Update views

---
+++ project_1/wiki/wiki/encyclopedia/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+
+def entry_info(request, title):
+    entry = util.get_entry(title)
+    context = {
+        'entry': entry,
+        'entry_name': title
+    }
+    return render(request, "encyclopedia/entry_info.html", context=context)
+
+
+def edit_entry(request, title):
+    entry = util.get_entry(title)
+    context = {
+        'entry': entry,
+        'entry_name': title
+    }
+    return render(request, "encyclopedia/edit_entry.html", context=context)
+
+
+def save_entry(request, title):
+    entry_dict = request.GET
+    content = entry_dict.get('entry_content')
+
+    util.save_entry(title, content)
+
+    return redirect('entry_info', title=title)
